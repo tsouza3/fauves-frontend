9918713ef276d41fea8ae49d34c9323e81eb525d
Souza <thiagodsouza.3@gmail.com>
2024-09-04 12:35:08 -0300

    fix

--- src/services/validateQrCode.js
+++ src/services/validateQrCode.js
@@ -1,11 +1,32 @@
-// src/services/qrCodeService.js
 import axios from 'axios';
 
-const apiBaseUrl = 'https://fauvesapi.thiagosouzadev.com'; // Substitua pela URL base da sua API
+const apiBaseUrl = 'https://fauvesapi.thiagosouzadev.com'; 
+
+const getTokenFromCookie = () => {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
+    "$1"
+  );
+};
 
 export const validateQrCode = async (qrCodeUrl) => {
   try {
-    const response = await axios.post(`${apiBaseUrl}/api/users/validate-qrcode`, { qrCodeUrl });
+    const token = getTokenFromCookie(); 
+
+    if (!token) {
+      throw new Error('Token não encontrado nos cookies');
+    }
+
+    const response = await axios.post(
+      `${apiBaseUrl}/api/users/validate-qrcode`,
+      { qrCodeUrl },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, 
+        },
+      }
+    );
+
     return response.data;
   } catch (error) {
     console.error('Erro ao validar o QR Code:', error);
